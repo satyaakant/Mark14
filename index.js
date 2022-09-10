97c4485a112dfcaec4303aa48efd46d29e3f1fd7
satyaaa <satyakantmishra50@gmail.com>
2022-09-10 14:28:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ var outputel= document.querySelector("#output");
 btnCheck.addEventListener('click',clickhandler);
 function calculateProfithandler(initial,quantity,current){
 if(initial>current){
-    var loss =(initial - current)*quantity;
+    var loss =(initial - current);
     var lossPercentage=(loss/initial)*100;
-    outputel.innerText=`hey the loss is ${loss} and the loss percentage is ${lossPercentage}`;
+    outputel.innerText=`hey the loss is ${loss} and the loss percentage is ${lossPercentage}%`;
 }
 else if(current>initial){
-    var profit=(current - initial)*quantity;
+    var profit=(current - initial);
     var profitPercentage=(profit/initial)*100;
-    outputel.innerText=`hey the profit is ${profit} and the profit percentage is ${profitPercentage}`;
+    outputel.innerText=`hey the profit is ${profit} and the profit percentage is ${profitPercentage}%`;
 }
 else{
 outputel.innerText=`No pain no gain and no gain no
