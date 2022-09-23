9758e7ae84cef58ad65dd7955682e4dd1a5bd5ae
satyaaa <satyakantmishra50@gmail.com>
2022-09-23 19:53:13 +0530

    index

--- index.js
+++ index.js
@@ -27,5 +27,10 @@ function clickhandler(){
     var ip =  Number( inputini.value);
     var qty=  Number( inputqua.value);
     var curr =Number( inputou.value);
+    if(ip==0){
+        outputel.innerText=`Entering number is least that we asked you to do:( `;
+    }
+    else{
     calculateProfithandler(ip,qty,curr);
+    }
 }
